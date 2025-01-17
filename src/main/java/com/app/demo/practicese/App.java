94159fefacbc6abecec5707e4b128ef0cecbc3b4
Nikshitha <Nikshitha@gmail.com>
2025-01-17 21:31:27 +0530

    demo

---
+++ src/main/java/com/app/demo/practicese/App.java
@@ -0,0 +1,10 @@
+package com.app.demo.practicese;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
